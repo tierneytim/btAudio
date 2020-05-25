bb63a2998d9a07017145a283c558e2af77c0b5cc
Tim Tierney <fiachmri@gmail.com>
2020-05-25 17:13:15 +0100

    templated filter

--- src/filter.cpp
+++ src/filter.cpp
@@ -7,25 +7,25 @@ filter::filter(float Fc,float fs, int ncascs,int type){
 		float K = tan(PI * Fc/fs);
 		float norm;
 		for(int i=0;i<ncascs;i++){
-			z1[i]=0.0;
-			z2[i]=0.0;
+			_z1[i]=0.0;
+			_z2[i]=0.0;
 			float Q= 1/(2*cos(PI/(ncascs*4)+i*PI/(ncascs*2)));
 			switch (type) {
 			 case lowpass:
 			    norm = 1 / (1 + K / Q + K * K);
-				b0[i] = K * K * norm;
-				b1[i] = 2 * b0[i];
-				b2[i] = b0[i];
-				a1[i] = 2 * (K * K - 1) * norm;
-				a2[i] = (1 - K / Q + K * K) * norm;
+				_b0[i] = K * K * norm;
+				_b1[i] = 2 * _b0[i];
+				_b2[i] = _b0[i];
+				_a1[i] = 2 * (K * K - 1) * norm;
+				_a2[i] = (1 - K / Q + K * K) * norm;
 				break;
              case highpass:
 				norm = 1 / (1 + K / Q + K * K);
-				b0[i] = 1 * norm;
-				b1[i] = -2 * b0[i];
-				b2[i] = b0[i];
-				a1[i] = 2 * (K * K - 1) * norm;
-				a2[i] = (1 - K / Q + K * K) * norm;
+				_b0[i] = 1 * norm;
+				_b1[i] = -2 * _b0[i];
+				_b2[i] = _b0[i];
+				_a1[i] = 2 * (K * K - 1) * norm;
+				_a2[i] = (1 - K / Q + K * K) * norm;
 				break;
 			}
 			
--- src/filter.h
+++ src/filter.h
@@ -12,21 +12,21 @@ public:
     filter(float Fc,float fs, int ncascs,int type);
 	
 	template <typename T> 
-	int16_t process(T in){
+	T process(T in){
 		float fout;
 		float fprev= (float)in;
 		for(int i=0; i<_ncascs;i++){
-			fout= fprev * b0[i] + z1[i];
-			z1[i] = fprev * b1[i] + z2[i] - a1[i] * fout;
-			z2[i] = fprev * b2[i] - a2[i] * fout;
+			fout= fprev * _b0[i] + _z1[i];
+			_z1[i] = fprev * _b1[i] + _z2[i] - _a1[i] * fout;
+			_z2[i] = fprev * _b2[i] - _a2[i] * fout;
 			fprev=fout;
 		}
-		return (int16_t)fout;
+		return fout;
 	}
 	
 
 private:
-	float b0[5], b1[5], b2[5], a1[5], a2[5],z1[5], z2[5];
+	float _b0[5], _b1[5], _b2[5], _a1[5], _a2[5], _z1[5], _z2[5];
 	int _ncascs;
 }; 
 #endif
