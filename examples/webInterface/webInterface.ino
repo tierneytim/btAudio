6d075a6bd84b4f958d2da9e4644ad3d04f19cc38
Tim Tierney <fiachmri@gmail.com>
2020-06-10 21:20:04 +0100

    webInterface and readme

--- examples/webInterface/webInterface.ino
+++ examples/webInterface/webInterface.ino
@@ -1,21 +1,31 @@
 #include<webDSP.h>
 #include<btAudio.h>
 
+// create audio object 
 btAudio audio = btAudio("ESP_Speaker");
+
+// create webserver object
 webDSP web;
 
 void setup() {
   Serial.begin(115200);  
+  
+  //start streaming audio
   audio.begin();
+  
+  //transmit data to DAC
   int bck = 26; 
   int ws = 27;
   int dout = 25;  
   audio.I2S(bck, dout, ws);
+  
+  // replace ssid and password with your details
   const char* ssid = "";
   const char* password = "";
   web.begin(ssid,password ,&audio); 
 }
 
 void loop() {
+  // continually check on client 
   web._server.handleClient();
 }
